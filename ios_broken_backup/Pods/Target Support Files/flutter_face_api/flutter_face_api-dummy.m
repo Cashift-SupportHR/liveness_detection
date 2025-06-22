91fa4d72077b6972816f2ad27244770353599f2c
mohamedabdelnaser <mohamedabdelnaser782@gmail.com>
2025-06-22 07:36:36 +0300

    build

--- ios_broken_backup/Pods/Target Support Files/flutter_face_api/flutter_face_api-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_flutter_face_api : NSObject
-@end
-@implementation PodsDummy_flutter_face_api
-@end
--- ios_broken_backup/Pods/Target Support Files/simple_native_image_compress/simple_native_image_compress-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_simple_native_image_compress : NSObject
-@end
-@implementation PodsDummy_simple_native_image_compress
-@end
