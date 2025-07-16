cb7ca119ffd555009591d72b8f6690882f108f41
Harby-SoftwareEngineer <anabassem5@gmail.com>
2025-07-16 12:16:10 +0400

    fix: update livelyness_detection dependency to use git source

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/mohamedabdelnaser/fvm/versions/3.29.0"
-export "FLUTTER_APPLICATION_PATH=/Users/mohamedabdelnaser/StudioProjects/cashift"
+export "FLUTTER_ROOT=E:\flutter_versions\flutter_windows_3.29.0-stable\flutter"
+export "FLUTTER_APPLICATION_PATH=E:\Working-Apps\chashift-projects\cashiftNew"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
